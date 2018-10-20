19bd29a43bc4da06ff43f1966a682ca095f11308
Yongho Choi <yongho1037@gmail.com>
2018-10-20 20:38:18 +0900

    오타 수정

--- elasticsearch/k8s-fabric8-custom/create.sh
+++ elasticsearch/k8s-fabric8-custom/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kubectl create sa elasticsearch && \
-kubectl create --save-config -f services/role.yml && \
+kubectl create --save-config -f accounts/role.yml && \
 kubectl create -f services/loadbalancer.yml && \
 kubectl create -f services/discovery.yml && \
 kubectl create -f deployments/es-master.yml && \
