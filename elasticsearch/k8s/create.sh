6c817fa79e012831bd071aa6ff1364901becf917
Yongho Choi <yongho1037@gmail.com>
2018-10-21 14:59:27 +0900

    스크립트 수정

--- elasticsearch/k8s/create.sh
+++ elasticsearch/k8s/create.sh
@@ -3,9 +3,8 @@ kubectl create sa elasticsearch && \
 kubectl create --save-config -f accounts/role.yml && \
 kubectl create -f services/loadbalancer.yml && \
 kubectl create -f services/discovery.yml && \
-sleep 10 && \
-kubectl create -f deployments/es-master.yml && \
-sleep 10 && \
+#kubectl create -f deployments/es-master.yml && \
+#sleep 10 && \
 kubectl create -f deployments/es-coord.yml && \
 sleep 10 && \
 kubectl create -f deployments/es-data.yml
--- elasticsearch/k8s/delete.sh
+++ elasticsearch/k8s/delete.sh
@@ -3,6 +3,6 @@ kubectl delete sa elasticsearch && \
 kubectl delete clusterrolebindings elasticsearch && \
 kubectl delete services elasticsearch && \
 kubectl delete services elasticsearch-cluster && \
-kubectl delete deployments elasticsearch-master && \
+#kubectl delete deployments elasticsearch-master && \
 kubectl delete deployments elasticsearch-coordinating-only && \
 kubectl delete deployments elasticsearch-data
